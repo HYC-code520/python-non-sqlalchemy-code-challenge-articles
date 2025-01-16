39697934f077d5af7004a63c540fd8ffdff404be
Ariel Chen <happilyeverafter0800@gmail.com>
2025-01-15 22:18:18 -0500

    Initializers and Properties: Finish author and magazine up to property name

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -8,6 +8,22 @@ class Author:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            print('Author name must be a string')
+        elif len(new_name) < 1:
+            print('Author name must be longer than 0 characters')
+        elif hasattr(self, '_name'):
+            print('name is a constant')
+        else:
+            self._name = new_name
+
+
     def articles(self):
         pass
 
@@ -24,6 +40,33 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            print('Magazine name must be a string')
+        elif not 2 <= len(new_name) <= 16:
+            print('Magazine name must be between 2 to 16 characters')
+        else:
+            self._name = new_name 
+
+    @property
+    def category(self)
+    return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if not isinstance(new_category, str):
+            print('Categories must be a string')
+        elif not len(new_category) > 0:
+            print('Categories must be longer than 0 characters')
+        else:
+            self._category = new_category
+
 
     def articles(self):
         pass
